7d1f889187fb2888cddebe924fb8bac46dd40d07
uchiha_vivek <vs160125@gmail.com>
2024-09-23 10:20:49 +0530

    reshape the matrix

---
+++ easy/__reshape-matrix.py
@@ -0,0 +1,11 @@
+# Write a Python function that reshapes a given matrix into a specified shape.
+import numpy as np
+def reshape_matrix(a:list[list[int|float]],new_shape: tuple[int|float]) -> list[list[int|float]]:
+    return np.array(a).reshape(new_shape).tolist()
+def main():
+    a=[[1,2,3,4],[5,6,7,8]]
+    new_shape = (4,2)
+    ans=reshape_matrix(a,new_shape)
+    print(ans)
+if __name__ == "__main__":
+    main() 
